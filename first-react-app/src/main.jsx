8c6000bb5bbe356a61b78444ef704da7c9615056
Ashmit Shukla <ashmitshukla18@gmail.com>
2024-01-06 23:41:46 +0530

    How react works based on understanding of our custom React

--- first-react-app/src/main.jsx
+++ first-react-app/src/main.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <>
+      <h1>
+        Custom React | Understanding of Working of React
+      </h1>
+    </>
+  )
+}
+
+const reactElement = {
+  type: 'a',
+  props: {
+        href: 'https://google.com',
+        target: '_blank'
+  },
+  children: 'Click me to visit Google'
+}
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
