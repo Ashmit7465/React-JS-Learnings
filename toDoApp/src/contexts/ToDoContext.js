8f082b8bcf4b7500ed1f810379deff32fdec35e3
Ashmit Shukla <ashmitshukla18@gmail.com>
2024-01-19 18:16:08 +0530

    ToDo App with context API and local storage concepts

---
+++ toDoApp/src/contexts/ToDoContext.js
@@ -0,0 +1,21 @@
+import {useContext, createContext} from "react"
+
+export const ToDoContext = createContext({
+      todos: [
+            {
+                  id: 1,
+                  toDo: "To Do task message",
+                  completed: false
+            }
+      ],
+      addToDo: (toDo) => {},
+      updateTodo: (id, toDo) => {},
+      deleteTodo: (id) => {},
+      toggleComplete: (id) => {}
+})
+
+export const useToDo = () => {
+      return useContext(ToDoContext);
+}
+
+export const ToDoContextProvider = ToDoContext.Provider
---
+++ toDoApp/src/contexts/index.js
@@ -0,0 +1,1 @@
+import { ToDoContext, ToDoContextProvider, useToDo } from "./ToDoContext";
